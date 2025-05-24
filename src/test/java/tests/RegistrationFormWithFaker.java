373279d458986138d2fbe779f203af3a3df441f1
july <julpkolp@gmail.com>
2025-05-24 20:55:10 +0300

    first commit

--- src/test/java/tests/RegistrationFormWithFaker.java
+++ src/test/java/tests/RegistrationFormWithFaker.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import pages.components.SummaryTableComponent;
@@ -8,6 +9,7 @@ import utils.FakerTestData;
 
 import java.util.Locale;
 
+import static io.qameta.allure.Allure.step;
 import static java.lang.Thread.sleep;
 
 public class RegistrationFormWithFaker extends TestBase {
@@ -16,67 +18,76 @@ public class RegistrationFormWithFaker extends TestBase {
     FakerTestData fakerTestData = new FakerTestData();
 
     @Test
+    @Tag("demoqa")
         //Проверка формы регистрации с заполнением всех полей
     void successRegistrationAllFilldsTest() {
-        registrationPage.openPage()
-                .bannersRemove()
-                .setFirstName(fakerTestData.firstName)
-                .setLastName(fakerTestData.lastName)
-                .setGender(fakerTestData.gender)
-                .setEmail(fakerTestData.email)
-                .setPhoneNumber(fakerTestData.phoneNumber)
-                .setDateOfBirth(fakerTestData.yearOfBirth, fakerTestData.monthOfBirth, fakerTestData.dayOfBirth)
-                .setSubjects(fakerTestData.subjects)
-                .setHobbies(fakerTestData.hobbies)
-                .setPicture(fakerTestData.picture)
-                .setCurrentAddress(fakerTestData.currentAddress)
-                .setState(fakerTestData.state)
-                .setCity(fakerTestData.city)
-                .buttonClick();
-
-
-        summaryTable.shouldAppear()
-                .checkFields("Student Name", fakerTestData.firstName + " " + fakerTestData.lastName)
-                .checkFields("Student Email", fakerTestData.email)
-                .checkFields("Gender", fakerTestData.gender)
-                .checkFields("Mobile", fakerTestData.phoneNumber)
-                .checkFields("Date of Birth", fakerTestData.dayOfBirth + " " + fakerTestData.monthOfBirth + ","
-                        + fakerTestData.yearOfBirth)
-                .checkFields("Subjects", fakerTestData.subjects)
-                .checkFields("Hobbies", fakerTestData.hobbies)
-                .checkFields("Picture", fakerTestData.picture)
-                .checkFields("Address", fakerTestData.currentAddress)
-                .checkFields("State and City", fakerTestData.state + " " + fakerTestData.city);
+        step("Открыть форму", () -> {
+            registrationPage.openPage()
+                    .bannersRemove();
+        });
+        step("Заполнить поля формы", () -> {
+            registrationPage.setFirstName(fakerTestData.firstName)
+                    .setLastName(fakerTestData.lastName)
+                    .setGender(fakerTestData.gender)
+                    .setEmail(fakerTestData.email)
+                    .setPhoneNumber(fakerTestData.phoneNumber)
+                    .setDateOfBirth(fakerTestData.yearOfBirth, fakerTestData.monthOfBirth, fakerTestData.dayOfBirth)
+                    .setSubjects(fakerTestData.subjects)
+                    .setHobbies(fakerTestData.hobbies)
+                    .setPicture(fakerTestData.picture)
+                    .setCurrentAddress(fakerTestData.currentAddress)
+                    .setState(fakerTestData.state)
+                    .setCity(fakerTestData.city);
+        });
+        step("Отправить форму", () -> {
+            registrationPage.buttonClick();
+        });
+        step("Проверить результат", () -> {
+            summaryTable.shouldAppear()
+                    .checkFields("Student Name", fakerTestData.firstName + " " + fakerTestData.lastName)
+                    .checkFields("Student Email", fakerTestData.email)
+                    .checkFields("Gender", fakerTestData.gender)
+                    .checkFields("Mobile", fakerTestData.phoneNumber)
+                    .checkFields("Date of Birth", fakerTestData.dayOfBirth + " " + fakerTestData.monthOfBirth + ","
+                            + fakerTestData.yearOfBirth)
+                    .checkFields("Subjects", fakerTestData.subjects)
+                    .checkFields("Hobbies", fakerTestData.hobbies)
+                    .checkFields("Picture", fakerTestData.picture)
+                    .checkFields("Address", fakerTestData.currentAddress)
+                    .checkFields("State and City", fakerTestData.state + " " + fakerTestData.city);
+        });
     }
 
-    @Test
+        @Test
+        @Tag("demoqa")
         //Проверка формы регистрации с заполнением только обязательных полей
-    void successRegistrationRequiredFilldsTest() {
-        registrationPage
-                .openPage()
-                .bannersRemove()
-                .setFirstName(fakerTestData.firstName)
-                .setLastName(fakerTestData.lastName)
-                .setGender(fakerTestData.gender)
-                .setPhoneNumber(fakerTestData.phoneNumber)
-                .buttonClick();
+        void successRegistrationRequiredFilldsTest(){
+            registrationPage
+                    .openPage()
+                    .bannersRemove()
+                    .setFirstName(fakerTestData.firstName)
+                    .setLastName(fakerTestData.lastName)
+                    .setGender(fakerTestData.gender)
+                    .setPhoneNumber(fakerTestData.phoneNumber)
+                    .buttonClick();
 
 
-        summaryTable.shouldAppear()
-                .checkFields("Student Name", fakerTestData.firstName + " " + fakerTestData.lastName)
-                .checkFields("Gender", fakerTestData.gender)
-                .checkFields("Mobile", fakerTestData.phoneNumber);
+            summaryTable.shouldAppear()
+                    .checkFields("Student Name", fakerTestData.firstName + " " + fakerTestData.lastName)
+                    .checkFields("Gender", fakerTestData.gender)
+                    .checkFields("Mobile", fakerTestData.phoneNumber);
 
-    }
+        }
 
-    @Test
+        @Test
+        @Tag("demoqa")
         //Проверка на то, что форма регистрации не отправляется если не заполнены обязательные поля
-    void negativeRegistrationTest() {
-        registrationPage
-                .openPage()
-                .bannersRemove()
-                .buttonClick();
+        void negativeRegistrationTest () {
+            registrationPage
+                    .openPage()
+                    .bannersRemove()
+                    .buttonClick();
 
-        summaryTable.shouldNotAppear();
+            summaryTable.shouldNotAppear();
+        }
     }
-}
